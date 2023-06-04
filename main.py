bd11a43c2ad24acd4730d9831c70e31c8cfa7699
Ilya Vasilev <bkmz1840@gmail.com>
2023-06-04 23:00:28 +0500

    feat: add Version 2

--- main.py
+++ main.py
@@ -1,161 +1,154 @@
-from scipy.optimize import minimize, Bounds, root
-from opt import *
+from functions import (
+    solve,
+    get_minimization_result,
+    get_mus,
+    get_start_data,
+    draw_result_plot,
+    draw_plot,
+)
+from opt_function import M_func
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 
-def optimize_main(data, params, M_r=None):
-    T = params["T"]
-    n = len(data)
-    mu_r = params["mu_r"]
-    H_r = params["H_r"]
-    ro_r = params["ro_r"]
-    mu_0 = params["mu_0"]
-    k_b = params["k_b"]
-    I = params["I"]
-    mu_min = params["mu_min"]
-    mu_step = params["mu_step"]
-    step = params["step"]
+# Args
+T = 290
+mu_0 = 1.256637
+k_b = 1.380649
+mu_r = 1
+ro_r = 10 ** 22
+H_r = 100
+M_r = None  # максимум по M
+n = None  # количество данных
+I = 6
+mu_min = 0.1
+mu_step = 1
+step = 0.2
 
-    input_data = form_input(data)
 
-    H = input_data[0]
-    M = input_data[1]
-
-    M_r = np.max(M) if M_r is None else M_r
-    print(f'M_r: {M_r}')
-
-    H_w = [h / H_r for h in H]
-    M_w = [m / M_r for m in M]
-
-    a = 10 ** 3 / M_r # mu_r * ro_r = 10^(-19) * 10^22 = 1000
-    b = (mu_0 * 10 ** (-2) * H_r) / (k_b * T) # Степени 10 сократились
-
-    mu_border = -10000
-    bounds = [
-        [0, 10],
-    ]
-    bounds.extend([[0, 1] for _ in range(I)])
-    print(f"Bounds: {bounds}")
-
-    constraints = ({'type': 'eq', "fun": constraint})
-    cur_mu_min = mu_min
-    result = []
-    not_success = False
-
-    while True:
-        if not not_success:
-            mus = get_mus(cur_mu_min, mu_step, I)
-
-            if mu_border == -10000:
-                mu_border = mus[1]
-
-            if abs(mu_border - mus[0]) <= 10 ** (-6):
-                print(f'BREAK: {mu_border} --- {mus[0]}')
-                break
-
-            print(f'm: {mus}')
-            cur_mu_min += step
-        else:
-            not_success = False
-
-        initial_guess = get_start_guess(I)
-        print(f'Init guess: {initial_guess}')
-
-        print('Start minimization')
-        fun = lambda p: optimization_func(p, mus, n, a, b, H_w, M_w)
-        res = minimize(
-            fun,
-            initial_guess,
-            bounds=bounds,
-            constraints=constraints
-        )
-        
-        print(f'Minimization is success: {res.success}')
-        if not res.success:
-            not_success = True
-            continue
-
-        print(f'Minimization result: {res.fun}')
-        found_params = list(res.x)[1:]
-        ro = res.x[0] * 10 ** 22
-        result.append(OptimizationResult(found_params, mus, ro))
-
-    div = 8 * I - 1
-    div1 = div // 2
-    header = "|" + " " * div1 + "mu" + " " * (div1 - 1) + "| |" + " " * div1 + "p" + " " * div1 + "| |   ro  |"
-    splitter = "|" + "-" * div + "| |" + "-" * div + "| |-------|"
-    print(header)
-    print(splitter)
-    for r in result:
-        print(r.form_line())
-    print(splitter)
-
-    H_t = []
-    M_t = []
-    backed_results = results_to_M(result, a, b, H_w, I)
-    backed_results_data = []
+def test():
+    H_w, _, M_r, a, b = get_start_data(H_r, mu_0, k_b, T)
     
-    for h, m in backed_results:
-        H_t.append(h)
-        M_t.append(m)
-        backed_results_data.append(f"{h} {m}")
+    test_mus = [1, 2, 3, 4, 5, 6]  # [mu_1, ..., mu_n]
+    test_params = [5, 0.06, 0.15, 0.32, 0.35, 0.1, 0.02]  # [ro, p_1, ..., p_n]
+    # test_mus = [1, 3, 5, 7, 9]
+    # test_params = [5, 0.62, 0.21, 0.12, 0.03, 0.02]
     
-    print("Backed results:")
-    print("\n".join(backed_results_data))
+    M_w = [M_func(test_params, a, b, test_mus, h) for h in H_w]
+    interations_count = 25
+    tries_count = 5
 
-    plt.grid()
-    plt.plot(H_w, M_w, color="black")
-    plt.plot(H_t, M_t, color="red")
-    plt.show()
+    backed_M = []
+    max_offsets = []
 
-    x = []
-    y = []
-    for r in result:
-        x.extend(map(lambda e: round(e, 2), r.mus))
-        y.extend(map(lambda e: round(e, 2), r.p))
+    for _ in range(tries_count):
+        val, x, __ = get_minimization_result({
+            'I': I,
+            'n': len(M_w),
+            'M_r': M_r,
+            'mu_0': mu_0,
+            'H_r': H_r,
+            'k_b': k_b,
+            'T': T,
+        }, test_mus, H_w, M_w, interations_count)
 
-    plt.grid()
-    plt.scatter(x, y)
-    plt.show()
+        print('*' * 11)
+        print(f'Min val: {val}')
+        print(f'Min x: ro - {x[0]}; p_i - {x[1:]}')
 
-
-def get_minimization_result(args, mus, H, M):
-    I = args['I']
-    n = args['n']
-    M_r = args['M_r']
-    mu_0 = args['mu_0']
-    H_r = args['H_r']
-    k_b = args['k_b']
-    T = args['T']
-
-    a = 10 ** 3 / M_r
-    b = (mu_0 * 10 ** (-2) * H_r) / (k_b * T)
-    
-    constraints = ({'type': 'eq', "fun": constraint})
-    bounds = [
-        [0, 100],
-    ]
-    bounds.extend([[0, 1] for _ in range(I)])
-    
-    while True:
-        initial_guess = get_start_guess(I)
-        
-        fun = lambda p: optimization_func(p, mus, n, a, b, H, M)
-        res = minimize(
-            fun,
-            initial_guess,
-            bounds=bounds,
-            constraints=constraints
-        )
-        print(f'Minimization is success: {res.success}')
-
-        if res.success:
-            print(f'Minimization result: {res.fun}')
+        max_offset = -1000
+        for i in range(1, len(test_params)):
+            offset = abs(test_params[i] - x[i])
             
-            found_params = list(res.x)[1:]
-            ro = res.x[0] * 10 ** 22
-            result = OptimizationResult(found_params, mus, ro)
+            if offset > max_offset:
+                max_offset = offset
 
-            return result
+        max_offsets.append(round(max_offset, 6))
+        back_M = [M_func(x, a, b, test_mus, h) for h in H_w]
+        backed_M.append(back_M)
+    
+    draw_result_plot(H_w, M_w, backed_M, max_offsets=max_offsets)
+
+
+def test_with_offset():
+    H_w, _, M_r, a, b = get_start_data(H_r, mu_0, k_b, T)
+    
+    # test_mus = [1, 2, 3, 4, 5, 6]  # [mu_1, ..., mu_n]
+    # test_params = [5, 0.06, 0.15, 0.32, 0.35, 0.1, 0.02]  # [ro, p_1, ..., p_n]
+    test_mus = [1, 2]
+    test_params = [5, 0.8, 0.2]
+    
+    M_w = [M_func(test_params, a, b, test_mus, h) for h in H_w]
+    
+    interations_count = 25
+    backed_M = []
+    border_mu = test_mus[1]
+    mus = test_mus
+    cur_mu_start = test_mus[0]
+    init_guess_pos = None
+    results = []
+    
+    muses = []
+
+    while True:
+        print(f'Positions: {init_guess_pos}')
+        
+        val, x, found_picked_places = get_minimization_result({
+            'I': I,
+            'n': len(M_w),
+            'M_r': M_r,
+            'mu_0': mu_0,
+            'H_r': H_r,
+            'k_b': k_b,
+            'T': T,
+        }, mus, H_w, M_w, interations_count, init_guess_pos=init_guess_pos)
+
+        print('*' * 11)
+        print(f'mu_i: {mus}')
+        print(f'Min val: {val}')
+        print(f'Min x: ro - {x[0]}; p_i - {x[1:]}')
+        print(f'Found picked places: {found_picked_places}')
+        
+        if init_guess_pos is None:
+            init_guess_pos = found_picked_places
+        
+        muses.append(mus)
+        
+        p = x[1:]
+        for i in range(len(mus)):
+            results.append((mus[i], p[i]))
+
+        back_M = [M_func(x, a, b, mus, h) for h in H_w]
+        backed_M.append(back_M)
+
+        cur_mu_start += step
+        mus = get_mus(cur_mu_start, mu_step, I)
+        
+        if abs(border_mu - mus[0]) <= 10 ** (-6):
+            break
+    
+    draw_result_plot(H_w, M_w, backed_M, muses=muses)
+    draw_plot(results, test_data=[test_mus, test_params[1:]])
+
+
+def main():
+    args = {
+        'T': T,
+        'mu_0': mu_0,
+        'k_b': k_b,
+        'mu_r': mu_r,
+        'ro_r': ro_r,
+        'H_r': H_r,
+        'I': I,
+        'mu_min': mu_min,
+        'mu_step': mu_step,
+        'step': step,
+    }
+    
+    solve(args)
+
+
+if __name__ == '__main__':
+    # test()  # Простой тест 
+    # test_with_offset()  # Тест со сдвигом mu_i
+    main()  # Реальные данные
